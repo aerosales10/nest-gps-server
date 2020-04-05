c7152e868da589723a8ea0cee9811b3774060a09
Alberto Rosales <aerosales10@gmail.com>
2020-04-05 23:59:28 +0000

    functions update

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -1,3 +1,6 @@
+// import * as crc from 'crc';
+// import { isString } from 'util';
+
 export function rads(x: number): number {
     return x * Math.PI / 180;
 };
@@ -15,3 +18,23 @@ export function str_pad(input: string, length: number, string: string) {
     input = input + '';
     return input.length >= length ? input : new Array(length - input.length + 1).join(string) + input;
 };
+
+// function hex_array_to_hex_str(hex_array) {
+//     var str = '';
+//     for (var i in hex_array) {
+//         var char;
+//         if (typeof (hex_array[i]) === 'number') char = hex_array[i].toString(16);
+//         else char = hex_array[i].toString();
+//         str += exports.str_pad(char, 2, '0');
+//     }
+//     return str;
+// };
+
+// export function crc_itu_get_verification(hex_data) {
+//     var crc16 = require('crc-itu').crc16;
+//     let str = "";
+
+//     if (isString(hex_data)) str = hex_data;
+//     else str = hex_array_to_hex_str(hex_data);
+//     return crc16(str, 'hex');
+// };
