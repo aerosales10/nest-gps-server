48b021f56f7b3035f616d542fcce2560415cd37e
Alberto Rosales <aerosales10@gmail.com>
2020-04-06 01:42:17 +0000

    Added module metadata

--- src/gps-server.module.ts
+++ src/gps-server.module.ts
@@ -3,30 +3,19 @@ import { GpsServerService } from './gps-server.service';
 import { DeviceFactory } from './factory';
 import { GpsServerOptionsInterface, GpsAdvancedOptionsInterface } from './interface';
 import { Echo } from './adapters';
+import { ModuleMetadata } from '@nestjs/common/interfaces';
 @Module({})
 export class GpsServerModule {
-  static register(options: GpsServerOptionsInterface, advanced_options?: GpsAdvancedOptionsInterface): DynamicModule {
+  static register(options: GpsServerOptionsInterface, metadata?: ModuleMetadata, advanced_options?: GpsAdvancedOptionsInterface): DynamicModule {
+    metadata.providers.push({ provide: 'GPS_CONFIG_OPTIONS', useValue: options });
+    metadata.providers.push({ provide: 'GPS_DEVICE_FACTORY', useClass: (advanced_options) ? advanced_options.device_factory : DeviceFactory });
+    metadata.providers.push({ provide: 'GPS_LOGGER', useValue: (advanced_options) ? advanced_options.logger : Logger });
+    metadata.providers.push({ provide: 'GPS_ADAPTER', useClass: options.adapter ?? Echo });
+    metadata.providers.push(GpsServerService);
+    metadata.exports.push(GpsServerService);
     return {
       module: GpsServerModule,
-      providers: [
-        GpsServerService,
-        {
-          provide: 'GPS_CONFIG_OPTIONS',
-          useValue: options
-        },
-        {
-          provide: 'GPS_DEVICE_FACTORY',
-          useClass: (advanced_options) ? advanced_options.device_factory : DeviceFactory
-        },
-        {
-          provide: 'GPS_LOGGER',
-          useValue: (advanced_options) ? advanced_options.logger : Logger
-        },
-        {
-          provide: 'GPS_ADAPTER',
-          useClass: options.adapter ?? Echo
-        }],
-      exports: [GpsServerService],
+      ...metadata
     };
   }
 }
